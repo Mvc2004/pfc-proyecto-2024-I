dd709bdd4879ec1e6393d4d93740b693011d909a
Mvc2004 <merly.velasquez@correounivalle.edu.co>
2024-06-21 22:28:47 -0500

    readyy

--- app/src/main/scala/proyecto/App.scala
+++ app/src/main/scala/proyecto/App.scala
@@ -1,8 +1,9 @@
+
 /**
-  * Taller 3 - Programación Funcional
-  * Autores: <Estudiantes>
-  * Profesor: Carlos A Delgado
-  */
+ * Taller 3 - Programación Funcional
+ * Autores: <Estudiantes>
+ * Profesor: Carlos A Delgado
+ */
 package proyecto
 import datos._
 
@@ -10,7 +11,7 @@ import org.scalameter.measure
 import org.scalameter.withWarmer
 import org.scalameter.Warmer
 
-object App{
+object App {
 
   def saludo() = "Proyecto final"
 
@@ -22,5 +23,19 @@ object App{
         (1 to 100000000).toArray
       }
     )
+    val itinerarioSeq = new Itinerario()
+    val itinerarioPar = new ItinerariosPar()
+    val pruebas = new BenchMark
+
+    val (seq1, par1) = pruebas.medirItinerarios(itinerarioSeq.itinerarios(vuelosC1, aeropuertos), itinerarioPar.itinerariosPar(vuelosC1, aeropuertos))
+    val (seq2, par2) = pruebas.medirItinerarios(itinerarioSeq.itinerariosTiempo(vuelosC1, aeropuertos), itinerarioPar.itinerariosTiempoPar(vuelosC1, aeropuertos))
+    val (seq3, par3) = pruebas.medirItinerarios(itinerarioSeq.itinerariosEscalas(vuelosC1, aeropuertos), itinerarioPar.itinerariosEscalasPar(vuelosC1, aeropuertos))
+    val (seq4, par4) = pruebas.medirItinerarios(itinerarioSeq.itinerariosAire(vuelosC1, aeropuertos), itinerarioPar.itinerariosAirePar(vuelosC1, aeropuertos))
+    val (seq5, par5) = pruebas.medirItinerariosSalida(itinerarioSeq.itinerariosSalida(vuelosC1, aeropuertos), itinerarioPar.itinerariosSalidaPar(vuelosC1, aeropuertos))
+    println(pruebas.mostrarResultado(seq1, par1, "itinerarios"))
+    println(pruebas.mostrarResultado(seq2, par2, "itinerariosTiempo"))
+    println(pruebas.mostrarResultado(seq3, par3, "itinerariosEscalas"))
+    println(pruebas.mostrarResultado(seq4, par4, "itinerariosAire"))
+    println(pruebas.mostrarResultado(seq5, par5, "itinerariosSalida"))
   }
- }
+}
--- app/src/main/scala/proyecto/BenchMark.scala
+++ app/src/main/scala/proyecto/BenchMark.scala
@@ -1,168 +1,35 @@
 package proyecto
-import datos._
-import org.scalameter._
 
-import org.scalameter.{Key, Warmer, withWarmer}
-
+import org.scalameter
+import org.scalameter.{Quantity, Warmer, measure, withWarmer}
 
 class BenchMark {
-
-  val objSecuencial = new Itinerario()
-  val objParalelo = new ItinerariosPar()
-
-  def itinerariosBenchMark(): Unit = {
-    val timeSecuencial = config(
-      Key.exec.minWarmupRuns := 500,
-      Key.exec.maxWarmupRuns := 500,
-      Key.exec.benchRuns := 20,
-      Key.verbose := false,
-    ) withWarmer new Warmer.Default measure {
-      objSecuencial.itinerarios(vuelosC1, aeropuertos)("PHX", "LAX")
+  def medirItinerarios(itinerarioSec: (String, String) => List[List[Vuelo]], itinerarioPar: (String, String) => List[List[Vuelo]]): (Quantity[Double], Quantity[Double]) = {
+    val secuencial = withWarmer(new Warmer.Default) measure {
+      itinerarioSec("DFW", "ATL")
     }
 
-    val timeParalela = config(
-      Key.exec.minWarmupRuns := 500,
-      Key.exec.maxWarmupRuns := 500,
-      Key.exec.benchRuns := 20,
-      Key.verbose := false,
-    ) withWarmer new Warmer.Default measure {
-      objParalelo.itinerariosPar(vuelosC1, aeropuertos)("PHX", "LAX")
+    val paralelo = withWarmer(new Warmer.Default) measure {
+      itinerarioPar("DFW", "ATL")
     }
 
-    println("Prueba de Itinerarios con Lista de Vuelos C1 y Lista Aeropuertos, Buscando El Vuelo de PHX a LAX")
-    println("Secuencial: " + timeSecuencial)
-    println("Paralela: " + timeParalela)
+    (secuencial, paralelo)
   }
 
-  def itinerariosTiempoBenchMark(): Unit = {
-
-    val timeSecuencial = config(
-      Key.exec.minWarmupRuns := 500,
-      Key.exec.maxWarmupRuns := 500,
-      Key.exec.benchRuns := 20,
-      Key.verbose := false,
-    ) withWarmer new Warmer.Default measure {
-      objSecuencial.itinerariosTiempo(vuelosD1, aeropuertos)("PHX", "LAX")
+  def medirItinerariosSalida(itinerarioSec: (String, String, Int, Int) => List[Vuelo], itinerarioPar: (String, String, Int, Int) => List[Vuelo]): (Quantity[Double], Quantity[Double]) = {
+    val secuencial = withWarmer(new Warmer.Default) measure {
+      itinerarioSec("DFW", "ATL", 16, 5)
     }
 
-    val timeParalela = config(
-      Key.exec.minWarmupRuns := 500,
-      Key.exec.maxWarmupRuns := 500,
-      Key.exec.benchRuns := 20,
-      Key.verbose := false,
-    ) withWarmer new Warmer.Default measure {
-      objParalelo.itinerariosTiempoPar(vuelosD1, aeropuertos)("PHX", "LAX")
+    val paralelo = withWarmer(new Warmer.Default) measure {
+      itinerarioPar("DFW", "ATL", 16, 5)
     }
 
-    println("Prueba Aeropuertos Curso Buscando El Vuelo ")
-    println("Secuencial: " + timeSecuencial)
-    println("Paralela: " + timeParalela)
-
+    (secuencial, paralelo)
   }
 
-  def itinerariosEscalasBenchMark(): Unit = {
-    val timeSecuencial = config(
-      Key.exec.minWarmupRuns := 500,
-      Key.exec.maxWarmupRuns := 500,
-      Key.exec.benchRuns := 20,
-      Key.verbose := false,
-    ) withWarmer new Warmer.Default measure {
-      objSecuencial.itinerariosEscalas(vuelosC1, aeropuertos)("PHX", "LAX")
-    }
-
-    val timeParalela = config(
-      Key.exec.minWarmupRuns := 500,
-      Key.exec.maxWarmupRuns := 500,
-      Key.exec.benchRuns := 20,
-      Key.verbose := false,
-    ) withWarmer new Warmer.Default measure {
-      objParalelo.itinerariosEscalasPar(vuelosC1, aeropuertos)("PHX", "LAX")
-    }
-
-    println("Prueba Aeropuertos Curso Buscando El Vuelo ")
-    println("Secuencial: " + timeSecuencial)
-    println("Paralela: " + timeParalela)
-
+  def mostrarResultado(secuencial: Quantity[Double], paralelo: Quantity[Double], nombreFuncion: String): String = {
+    val aceleracion = secuencial.value / paralelo.value
+    s"Tiempo de $nombreFuncion: Secuencial: $secuencial, Paralelo: $paralelo, Aceleración: $aceleracion"
   }
-
-  def itinerariosAireBenchMark(): Unit = {
-
-    val timeSecuencial = config(
-      Key.exec.minWarmupRuns := 500,
-      Key.exec.maxWarmupRuns := 500,
-      Key.exec.benchRuns := 20,
-      Key.verbose := false,
-    ) withWarmer new Warmer.Default measure {
-      objSecuencial.itinerariosAire(vuelosC1, aeropuertos)("PHX", "LAX")
-    }
-
-    val timeParalela = config(
-      Key.exec.minWarmupRuns := 500,
-      Key.exec.maxWarmupRuns := 500,
-      Key.exec.benchRuns := 20,
-      Key.verbose := false,
-    ) withWarmer new Warmer.Default measure {
-      objParalelo.itinerariosAirePar(vuelosC1, aeropuertos)("PHX", "LAX")
-    }
-
-    println("Prueba Aeropuertos Curso Buscando El Vuelo ")
-    println("Secuencial: " + timeSecuencial)
-    println("Paralela: " + timeParalela)
-
-  }
-
-  def itinerariosSalidaBenchMark(): Unit = {
-
-    val timeSecuencial = config(
-      Key.exec.minWarmupRuns := 500,
-      Key.exec.maxWarmupRuns := 500,
-      Key.exec.benchRuns := 20,
-      Key.verbose := false,
-    ) withWarmer new Warmer.Default measure {
-      objSecuencial.itinerariosSalida(vuelosC1, aeropuertos)("DFW", "ATL", 15, 10)
-      objSecuencial.itinerariosSalida(vuelosC1, aeropuertos)("ATL", "DFW", 15, 10)
-    }
-
-    val timeParalela = config(
-      Key.exec.minWarmupRuns := 500,
-      Key.exec.maxWarmupRuns := 500,
-      Key.exec.benchRuns := 20,
-      Key.verbose := false,
-    ) withWarmer new Warmer.Default measure {
-      objParalelo.itinerariosSalidaPar(vuelosC1, aeropuertos)("DFW", "ATL", 15, 10)
-      objParalelo.itinerariosSalidaPar(vuelosC1, aeropuertos)("ATL", "DFW", 15, 10)
-    }
-
-    println("Prueba Aeropuertos Curso Buscando El Vuelo ")
-    println("Secuencial: " + timeSecuencial)
-    println("Paralela: " + timeParalela)
-
-  }
-  def itinerariosSalidaBenchmark(): Unit = {
-
-    val timeParalela5 = config(
-      Key.exec.minWarmupRuns := 100,
-      Key.exec.maxWarmupRuns := 100,
-      Key.exec.benchRuns := 20,
-      Key.verbose := false,
-    ) withWarmer new Warmer.Default measure {
-
-
-      objParalelo.itinerariosSalidaPar(vuelosC1, aeropuertos)("BOS","SFO", 12, 22)
-    }
-    val timeSecuencial5 = config(
-
-      Key.exec.minWarmupRuns := 100,
-      Key.exec.maxWarmupRuns := 100,
-      Key.exec.benchRuns := 20,
-      Key.verbose := false,
-    ) withWarmer new Warmer.Default measure {
-      objSecuencial.itinerariosSalida(vuelosCurso , aeropuertosCurso)
-      objSecuencial.itinerariosSalida(vuelosC1, aeropuertos)("BOS","SFO", 12, 22)
-      objSecuencial.itinerariosSalida(vuelosC1, aeropuertos)("BOS","SFO", 12, 22)
-    }
-    println(s"Tiempo de ejecución Salida P5: $timeParalela5")
-    println(s"Tiempo de ejecución Salida S5: $timeSecuencial5")
-  }
-
 }
